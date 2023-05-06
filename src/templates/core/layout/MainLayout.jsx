59fae27f1f196e902edb0470af4307ff8a06e853
Gabul99 <hclee99@kaist.ac.kr>
2023-05-06 22:45:04 +0900

    MainTopBar 만듦

--- src/templates/core/layout/MainLayout.jsx
+++ src/templates/core/layout/MainLayout.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 import MainLeftNavigationBar from '../../main/MainLeftNavigationBar';
+import MainTopBar from '../../main/MainTopBar';
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+`;
+
+const Body = styled.div`
+  width: 100%;
+  flex-grow: 1;
 `;
 
 const OutletArea = styled.div`
@@ -18,10 +24,13 @@ const OutletArea = styled.div`
 const MainLayout = () => {
   return (
     <Container>
-      <MainLeftNavigationBar />
-      <OutletArea>
-        <Outlet />
-      </OutletArea>
+      <MainTopBar />
+      <Body>
+        <MainLeftNavigationBar />
+        <OutletArea>
+          <Outlet />
+        </OutletArea>
+      </Body>
     </Container>
   );
 };
