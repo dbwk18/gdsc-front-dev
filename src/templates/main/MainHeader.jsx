ac4ae6c3430d8460973c2ee91903491c04073e81
오주원 <allisaoh@kaist.ac.kr>
2023-05-19 14:00:22 +0900

    feat: header에 로고 추가

--- src/templates/main/MainHeader.jsx
+++ src/templates/main/MainHeader.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-import BAILogo from 'public/assets/KAIST_BAI_logo.svg';
+import logo from '../../assets/KAIST_BAI_logo.svg';
 
 const Container = styled.div`
   width: 100%;
-  height: 134px;
+  height: 72px;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   align-items: center;
-  padding: 0 44px;
+  padding: 0 40px;
   flex-shrink: 0;
+
+  .logo {
+    width: 113px;
+    height: 21px;
+  }
 `;
 
 const MainHeader = () => {
-  return <Container>KAIST BAI</Container>;
+  return (
+    <Container>
+      <div className="logo">
+        <img src={logo} alt="KAIST BAI logo" />
+      </div>
+      KAIST 학부 총학생회
+    </Container>
+  );
 };
 
 export default MainHeader;
