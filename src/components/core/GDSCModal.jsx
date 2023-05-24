382a14771a891bcab8c284c68108dcc871a83349
Gabul99 <hclee99@kaist.ac.kr>
2023-05-25 00:01:27 +0900

    fix: GDSCModal Wrapper background color 삭제

--- src/components/core/GDSCModal.jsx
+++ src/components/core/GDSCModal.jsx
@@ -1,9 +1,7 @@
 import { Modal } from '@mui/material';
 import styled from 'styled-components';
-import Colors from '../../style/Colors';
 
 const Wrapper = styled.div`
-  background-color: ${Colors.WHITE100};
   position: absolute;
   top: 50%;
   left: 50%;
