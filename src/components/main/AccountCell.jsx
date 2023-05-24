7be0e37a6e821eaf2a42d486e6ff1f3c14e1be41
Yumin Choi <yuminchoi@Yumins-MacBook-Pro.local>
2023-05-24 23:01:12 +0900

    font color

--- src/components/main/AccountCell.jsx
+++ src/components/main/AccountCell.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TableRow, TableCell } from '@mui/material';
 import GDSCText from '../core/GDSCText';
+import Colors from '../../style/Colors';
 
 const AccountCell = ({
   businessAt,
@@ -21,43 +22,67 @@ const AccountCell = ({
   return (
     <TableRow>
       <TableCell>
-        <GDSCText size={13}>{businessAt}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {businessAt}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{manager}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {manager}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={10}>{itemSub}</GDSCText>
+        <GDSCText size={10} color={Colors.GREY80}>
+          {itemSub}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{itemCode}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {itemCode}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{transactionType}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {transactionType}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{income}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {income}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{expense}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {expense}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{balance}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {balance}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{transactionAt}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {transactionAt}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={10}>
+        <GDSCText size={10} color={Colors.GREY80}>
           ({bankName}-{accountHolder})
         </GDSCText>
-        <GDSCText size={10}>{accountNumber}</GDSCText>
+        <GDSCText size={10} color={Colors.GREY80}>
+          {accountNumber}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{receipts}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {receipts}
+        </GDSCText>
       </TableCell>
       <TableCell>
-        <GDSCText size={13}>{remarks}</GDSCText>
+        <GDSCText size={13} color={Colors.GREY80}>
+          {remarks}
+        </GDSCText>
       </TableCell>
     </TableRow>
   );
