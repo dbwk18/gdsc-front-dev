fa676d3b4f08463850e7f5716602d83b33cf14cd
Jio Jang <talktalk0917@gmail.com>
2023-05-29 11:15:25 +0900

    changed tap naming rules

--- src/components/main/budget/TabPanels.jsx
+++ src/components/main/budget/TabPanels.jsx
@@ -7,29 +7,35 @@ import ExpenseTable from './ExpenseTable';
 import SettlementTable from './SettlementTable';
 import Panel from './Panel';
 
+const TapType = {
+  INCOME: 'INCOME',
+  EXPENSE: 'EXPENSE',
+  SETTLEMENT: 'SETTLEMENT',
+};
+
 const TabPanels = () => {
-  const [tap, currTap] = React.useState(0);
+  const [currTap, setCurrTap] = React.useState(0);
 
   const handleChange = (event, newTap) => {
-    currTap(newTap);
+    setCurrTap(newTap);
   };
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={tap} onChange={handleChange} aria-label="basic tabs example">
+        <Tabs value={currTap} onChange={handleChange} aria-label="basic tabs example">
           <Tab sx={{ color: 'red', fontWeight: 'bold' }} label="예산" />
           <Tab sx={{ color: 'red', fontWeight: 'bold' }} label="지출" />
           <Tab sx={{ color: 'red', fontWeight: 'bold' }} label="결산" />
         </Tabs>
       </Box>
-      <Panel value={tap} index={0}>
+      <Panel value={currTap} index={TapType.INCOME}>
         <IncomeTable />
       </Panel>
-      <Panel value={tap} index={1}>
+      <Panel value={currTap} index={TapType.EXPENSE}>
         <ExpenseTable />
       </Panel>
-      <Panel value={tap} index={2}>
+      <Panel value={currTap} index={TapType.SETTLEMENT}>
         <SettlementTable />
       </Panel>
     </Box>
