872612370c5b6ee03d45b5908e7c8aa7277c6504
choseongje <hangoy0606@gmail.com>
2023-05-29 11:50:36 +0900

    feat: BudgetModal layout

--- src/components/main/budget/BudgetModal.jsx
+++ src/components/main/budget/BudgetModal.jsx
@@ -1,5 +1,42 @@
+import styled from 'styled-components';
+import Colors from '../../../style/Colors';
+import GDSCText, { TextType } from '../../core/GDSCText';
+import GDSCButton from '../../core/GDSCButton';
+
+const Container = styled.div`
+  width: 1088px;
+  height: 638px;
+  display: flex;
+  border-radius: 20px;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 72px;
+  border-radius: 20px 20px 0px 0px;
+  padding: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const BudgetModal = () => {
-  return <div>hi</div>;
+  return (
+    <Container style={{ backgroundColor: Colors.WHITE100 }}>
+      <Header style={{ backgroundColor: Colors.BLUE_DEEP }}>
+        <GDSCText size={20} fontType={TextType.BOLD} color={Colors.WHITE100}>
+          예결산안 항목 추가
+        </GDSCText>
+        <GDSCText size={20} fontType={TextType.BOLD} color={Colors.WHITE100}>
+          X
+        </GDSCText>
+      </Header>
+      <div>hello</div>
+    </Container>
+  );
 };
 
 export default BudgetModal;
