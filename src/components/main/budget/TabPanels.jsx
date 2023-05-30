9e0e9574670d06d408a522dfd9a4c0052d950d3d
Jio Jang <talktalk0917@gmail.com>
2023-05-30 21:56:11 +0900

    test

--- src/components/main/budget/TabPanels.jsx
+++ src/components/main/budget/TabPanels.jsx
@@ -12,34 +12,3 @@ const TabType = {
   EXPENSE: 'EXPENSE',
   SETTLEMENT: 'SETTLEMENT',
 };
-
-const TabPanels = () => {
-  const [currTab, setCurrTab] = React.useState(0);
-
-  const handleChange = (event, newTab) => {
-    setCurrTab(newTab);
-  };
-
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={currTab} onChange={handleChange} aria-label="basic tabs example">
-          <Tab sx={{ color: 'red', fontWeight: 'bold' }} label="예산" />
-          <Tab sx={{ color: 'red', fontWeight: 'bold' }} label="지출" />
-          <Tab sx={{ color: 'red', fontWeight: 'bold' }} label="결산" />
-        </Tabs>
-      </Box>
-      <Panel value={currTab} index={0}>
-        <IncomeTable />
-      </Panel>
-      <Panel value={currTab} index={1}>
-        <ExpenseTable />
-      </Panel>
-      <Panel value={currTab} index={2}>
-        <SettlementTable />
-      </Panel>
-    </Box>
-  );
-};
-
-export default TabPanels;
