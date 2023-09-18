9434600ee21b1fe9b472100b720e8b46c12b36ed
Yoojin Hong <dbwk18@kaist.ac.kr>
2023-09-19 00:56:41 +0900

    feature: add header logo

--- src/templates/main/MainHeader.jsx
+++ src/templates/main/MainHeader.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '../../style/img/KAIST_BAI_Logo.svg';
 
 const Container = styled.div`
   width: 100%;
-  height: 134px;
+  height: 72px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 0 44px;
+  padding: 0 40px;
   flex-shrink: 0;
 `;
 
 const MainHeader = () => {
-  return <Container>KAIST BAI</Container>;
+  return (
+    <Container>
+      <img src={logo} width="120px" alt="logo" />
+    </Container>
+  );
 };
 
 export default MainHeader;
