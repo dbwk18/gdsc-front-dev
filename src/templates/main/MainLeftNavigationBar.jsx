6078db259989b8ff9e82c3f5f4353e621bef3633
JaeeunKim02 <kumi1010@naver.com>
2023-12-20 21:19:56 +0900

    leftNavigationBar1

--- src/templates/main/MainLeftNavigationBar.jsx
+++ src/templates/main/MainLeftNavigationBar.jsx
@@ -1,19 +1,81 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useLocation, useNavigate } from 'react-router-dom';
 import Colors from '../../style/Colors';
-import LNBItem from '../../components/main/LNBItem';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Menu from '../../components/main/Menu';
+import KaistBaiLogo from '../../style/img/KAIST_BAI.svg';
+import ArrowBlack from '../../style/img/arrow-back-double.svg';
+import ClipboardBlack from '../../style/img/clipboard.svg';
+import DollarBlack from '../../style/img/dollar-1.svg';
+import HomeBlack from '../../style/img/home-1.svg';
+import Icecream from '../../style/img/icecream.svg';
+import SettingsBlack from '../../style/img/settings.svg';
+import UserSquareBlack from '../../style/img/user-square.svg';
+// 파란색 아이콘
+import ArrowBlue from '../../style/img/arrow-back-double-blue.svg';
+import ClipboardBlue from '../../style/img/clipboard-blue.svg';
+import DollarBlue from '../../style/img/dollar-blue.svg';
+import HomeBlue from '../../style/img/home-blue.svg';
+import SettingsBlue from '../../style/img/settings-blue.svg';
+import UserSquareBlue from '../../style/img/user-square-blue.svg';
 
 const Container = styled.div`
-  width: 340px;
-  height: 100%;
   display: flex;
+  height: 1024px;
   flex-direction: column;
-  flex-shrink: 0;
-  padding: 26px;
+  justify-content: space-between;
+  align-items: flex-start;
+  background: ${Colors.WHITE100};
+`;
+
+const Frame488 = styled.div`
+  display: flex;
+  padding-top: 16px;
+  flex-direction: column;
+  align-items: flex-start;
   gap: 8px;
-  background-color: ${Colors.BLUE_BACKGROUND};
-  border-right: 1px solid ${Colors.BLACK08};
+`;
+const KaistBai = styled.div`
+  display: flex;
+  height: 64px;
+  padding: 0px 24px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+`;
+const Frame492 = styled.div`
+  display: flex;
+  width: 200px;
+  padding: 8px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .Chip {
+    display: flex;
+    height: 40px;
+    padding: 10px 20px 10px 16px;
+    align-items: center;
+    gap: 8px;
+    align-self: stretch;
+    border-radius: 21px;
+    background: #e9eaed;
+  }
+  .ChipName {
+    color: #223047;
+    font-family: Pretendard Variable;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 20px;
+  }
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  align-self: stretch;
+  background-color: rgba(216, 220, 224, 1);
 `;
 
 const MainLeftNavigationBar = () => {
@@ -22,16 +84,35 @@ const MainLeftNavigationBar = () => {
 
   return (
     <Container>
-      <LNBItem
-        label="예결산안"
-        selected={location.pathname.includes('budget')}
-        onClick={() => navigate('/main/budget')}
-      />
-      <LNBItem
-        label="통장거래내역"
-        selected={location.pathname.includes('account')}
-        onClick={() => navigate('/main/account')}
-      />
+      <Frame488>
+        <KaistBai>
+          <img src={KaistBaiLogo} width="116" height="20" alt="logo" />
+        </KaistBai>
+        <Frame492>
+          <div className="Chip">
+            <img src={Icecream} width="20" height="20" alt="icecream" />
+            <p className="ChipName">감사원</p>
+          </div>
+        </Frame492>
+        <Menu iconBlack={HomeBlack} iconBlue={HomeBlue} menuName="홈" />
+        <Menu iconBlack={UserSquareBlack} iconBlue={UserSquareBlue} menuName="피감기구 계정관리" />
+        <Divider />
+        <Menu
+          iconBlack={DollarBlack}
+          iconBlue={DollarBlue}
+          menuName="예결산안"
+          onClick={() => navigate('/main/budget')}
+        />
+        <Menu
+          iconBlack={ClipboardBlack}
+          iconBlue={ClipboardBlue}
+          menuName="통장거래내역"
+          onClick={() => navigate('/main/account')}
+        />
+        <Divider />
+        <Menu iconBlack={SettingsBlack} iconBlue={SettingsBlue} menuName="설정" />
+      </Frame488>
+      <Menu iconBlack={ArrowBlack} iconBlue={ArrowBlue} menuName="접기" />
     </Container>
   );
 };
