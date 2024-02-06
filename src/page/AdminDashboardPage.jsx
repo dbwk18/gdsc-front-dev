5ec05326d6d185b708322c557e6edaa93d0a6d7a
Sehyeon Kim <sue2368@gmail.com>
2024-02-06 16:30:07 +0900

    time range error fixed

--- src/page/AdminDashboardPage.jsx
+++ src/page/AdminDashboardPage.jsx
@@ -64,7 +64,9 @@ const AdminDashboardPage = () => {
         const selected = ablePeriod[0];
         setTargetYear(selected.year);
         setTargetHalf(selected.half);
-        setFormattedPeriod(convertPeriodToLongFormat(selected.start, selected.end));
+        const formatted = convertPeriodToLongFormat(selected.start, selected.end);
+        console.log(formatted);
+        setFormattedPeriod(formatted);
       }
     });
   }, []);
